6166a6730561ce8d2f189a40e1b9b26e3cb739cf
Danisfabric <danisfabric@gmail.com>
2016-06-29 15:55:09 +0800

    Initial Commit

---
+++ FleaSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FleaSample
+//
+//  Created by 廖雷 on 16/6/29.
+//  Copyright © 2016年 廖雷. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
